26ae787fbeed3c4178620d9be9cf9ce21f29e11d
LizRadds <avilradish@gmail.com>
2023-02-26 22:51:28 +0100

    init: sstart guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.check = this.max - Math.floor((this.max - this.min) / 2);
+        return this.check;
     }
 
     lower() {
-
+        this.max =  this.check;
     }
 
     greater() {
-
+        this.min =  this.check;
     }
 }
 
